d8b5a4992ea6d4361b40be7a9ac688d4d0c9ab24
ZaidanIZ <zaidan.izzuddin.24@gmail.com>
2022-09-05 21:46:16 +0700

    select target

---
+++ unity/select target/Assets/Target.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+	renderer.material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+}
