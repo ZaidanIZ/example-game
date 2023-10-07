6969dd093523a5c00757dd69ada81a2a21b76652
ZaidanIZ <zaidan.izzuddin.24@gmail.com>
2023-10-07 10:43:21 +0700

    crossy zombie

---
+++ unity/CrossyZombie_SourceCode/CrossyZombie_SourceCode/Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+﻿
+public class Constants
+{
+    public const string FIRST_TIME_PLAY = "firstTime";
+    public const string SELECTED_CHARACTER = "selectedChar";
+    public const string BEST_SCORE = "bestScore";
+    public const string COIN = "coin";
+    public const string DIAMOND = "diamond";
+    public const string STATE_DECREASE_TIME = "stateDTime";
+    public const string STATE_ZOMBIE_TIME = "stateZTime";
+    public const string MUSIC = "music";
+    public const string SOUND = "sound";
+    public const string NOTIFY = "notify";
+}
---
+++ unity/CrossyZombie_SourceCode/CrossyZombie_SourceCode/Assets/Scripts/SortOrderSprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortOrderSprite : MonoBehaviour
+{
+    [Tooltip("Value increases or decreases to sort order of spriteRenderer")]
+    public int parameter;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!GameController.Instance.GameOver)
+        {
+            if (col.tag == "Check" || col.tag == "ItemCheck")
+            {
+                if (col.transform.parent.transform.position.y <= transform.position.y)
+                {
+                    spriteRenderer.sortingOrder = col.GetComponentInParent<SpriteRenderer>().sortingOrder - parameter;
+                }
+                else
+                {
+                    spriteRenderer.sortingOrder = col.GetComponentInParent<SpriteRenderer>().sortingOrder + parameter;
+                }
+            }
+        }
+    }
+}
